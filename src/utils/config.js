294c431815176f490f282530dbb432fccd9e3161
wangqiang <296361319@qq.com>
2018-12-21 19:30:07 +0800

    提交

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 
-// let api='https://testzlgj.zgftlm.com';
+let api='https://teeapi.zgftlm.com';
 //    let api='http://192.168.10.102';
-// let front='http://xcxadmin.zgftlm.com';
+let front='http://teeadmin.zgftlm.com';
 //  let api='http://192.168.10.105';
- let front='http://192.168.10.105:3000';
-    let api='http://192.168.10.105';
+//  let front='http://192.168.1.9:3000';
+//     let api='http://192.168.1.9';
 
 module.exports = {
     api: api+'/public/meeting/',
